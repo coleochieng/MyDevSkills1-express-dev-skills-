a793a5f3edda09fa813e64bf4992132e7dc55592
coleochieng <nicole.ochieng98@gmail.com>
2022-07-15 21:30:27 -0500

    update multiple files

--- routes/skills.js
+++ routes/skills.js
@@ -10,14 +10,9 @@ router.get('/new', skillsCtrl.new);
 
 router.get('/:id', skillsCtrl.show);
 
-router.get('/:id/edit', skillsCtrl.edit); 
-
 router.post('/', skillsCtrl.create);
 
 router.delete('/:id', skillsCtrl.delete);
-
-
-
 
 
 
