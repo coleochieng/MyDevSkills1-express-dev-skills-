abab8d2e475a8c28846ebcc3cf1286d21160342e
coleochieng <nicole.ochieng98@gmail.com>
2022-07-15 20:16:04 -0500

    update routes > skills.js

---
+++ routes/skills.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const todosCtrl = require('../controllers/todos');
+
+
+router.get('/', todosCtrl.index);
+
+router.get('/new', todosCtrl.new);
+
+router.get('/:id', todosCtrl.show);
+
+router.get('/:id/edit', todosCtrl.edit); 
+
+router.post('/', todosCtrl.create);
+
+router.delete('/:id', todosCtrl.delete);
+
+
+
+
+
+
+module.exports = router;
